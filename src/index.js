dab3e00d36f2d7f6f423d87e1a945cd88292257b
terminatorDX <jain.cj.chetan@gmail.com>
2019-08-14 20:52:14 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -27,12 +27,12 @@ import AdminLayout from "layouts/Admin.jsx";
 import AuthLayout from "layouts/Auth.jsx";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
-      <Route path="/auth" render={props => <AuthLayout {...props} />} />
-      <Redirect from="/" to="/admin/index" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <Switch>
+            <Route path="/admin" render={props => <AdminLayout {...props} />} />
+            <Route path="/auth" render={props => <AuthLayout {...props} />} />
+            <Redirect from="/" to="/admin/index" />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
