c43140594a43300b408277c2989ee2ed88c9d26a
terminatorDX <jain.cj.chetan@gmail.com>
2019-08-15 14:13:40 +0530

    removed charts and auth

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,6 @@
 import Index from "views/Index.jsx";
 import Profile from "views/examples/Profile.jsx";
 import Maps from "views/examples/Maps.jsx";
-import Register from "views/examples/Register.jsx";
-import Login from "views/examples/Login.jsx";
 import Tables from "views/examples/Tables.jsx";
 import Icons from "views/examples/Icons.jsx";
 
@@ -41,20 +39,6 @@ var routes = [
         icon: "ni ni-bullet-list-67 text-red",
         component: Tables,
         layout: "/admin"
-    },
-    {
-        path: "/login",
-        name: "Login",
-        icon: "ni ni-key-25 text-info",
-        component: Login,
-        layout: "/auth"
-    },
-    {
-        path: "/register",
-        name: "Register",
-        icon: "ni ni-circle-08 text-pink",
-        component: Register,
-        layout: "/auth"
     }
 ];
 export default routes;
