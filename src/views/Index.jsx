0d1caf1803bf125bbc46c340c6a6c231ef78d468
terminatorDX <jain.cj.chetan@gmail.com>
2019-08-15 14:41:32 +0530

    removed charts and remenants of charts

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -1,63 +1,21 @@
 import React from "react";
-// node.js library that concatenates classes (strings)
-import classnames from "classnames";
-// javascipt plugin for creating charts
-import Chart from "chart.js";
-// react plugin used to create charts
-import { Line, Bar } from "react-chartjs-2";
-// reactstrap components
-import {
-    Button,
-    Card,
-    CardHeader,
-    CardBody,
-    NavItem,
-    NavLink,
-    Nav,
-    Container,
-    Row,
-    Col
-} from "reactstrap";
-
-// core components
-import {
-    chartOptions,
-    parseOptions,
-    chartExample1
-} from "variables/charts.jsx";
 
 import Header from "components/Headers/Header.jsx";
 
 class Index extends React.Component {
     state = {
-        activeNav: 1,
-        chartExample1Data: "data1"
+        activeNav: 1
     };
     toggleNavs = (e, index) => {
         e.preventDefault();
         this.setState({
-            activeNav: index,
-            chartExample1Data:
-                this.state.chartExample1Data === "data1" ? "data2" : "data1"
+            activeNav: index
         });
-        let wow = () => {
-            console.log(this.state);
-        };
-        wow.bind(this);
-        setTimeout(() => wow(), 1000);
-        // this.chartReference.update();
     };
-    componentWillMount() {
-        if (window.Chart) {
-            parseOptions(Chart, chartOptions());
-        }
-    }
     render() {
         return (
             <>
                 <Header />
-                {/* Page content */}
-                <Container className="mt--7" fluid />
             </>
         );
     }
